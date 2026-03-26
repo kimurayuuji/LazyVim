c12168d66824996e8cb581a684b3834d31043bc8
Yuji Kimura <kimurayuuji79@gmail.com>
2026-03-26 16:03:56 +0900

    modify

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -34,9 +34,9 @@ vim.keymap.set("n", "<C-w>-", "<C-w>s", { desc = "Horizontal Split" })
 vim.keymap.set("n", "<C-w>|", "<C-w>v", { desc = "Vertical Split" })
 vim.keymap.set("n", "<C-w>x", "<C-w>c", { desc = "Close split pane" })
 
-
 -- VSCode
 if vim.g.vscode then
-  vim.keymap.set('n', 'gr', [[<Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>]])
-  vim.keymap.set('n', 'gR', [[<Cmd>call VSCodeNotify('references-view.findReferences')<CR>]])
+  vim.keymap.set("n", "gr", [[<Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>]])
+  vim.keymap.set("n", "gR", [[<Cmd>call VSCodeNotify('references-view.findReferences')<CR>]])
+  vim.keymap.set("n", "za", [[<Cmd>call VSCodeNotify('editor.toggleFold')<CR>]])
 end
