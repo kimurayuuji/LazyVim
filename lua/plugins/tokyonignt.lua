0d649a6c56ce5dc0334b97de96b1628cf8c8944e
Yuji Kimura <kimurayuuji79@gmail.com>
2026-03-30 13:31:17 +0900

    modify

--- lua/plugins/tokyonignt.lua
+++ lua/plugins/tokyonignt.lua
@@ -3,6 +3,25 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
+  keys = {
+    {
+      "<leader>ut",
+      function()
+        local tn = require("tokyonight")
+        local config = require("tokyonight.config")
+        local new_transparent = not config.options.transparent
+        tn.setup({
+          transparent = new_transparent,
+          styles = {
+            sidebars = new_transparent and "transparent" or "dark",
+            floats = new_transparent and "transparent" or "dark",
+          },
+        })
+        vim.cmd("colorscheme tokyonight")
+      end,
+      desc = "Toggle Transparency",
+    },
+  },
   ---@module 'tokyonight'
   ---@type tokyonight.Config
   opts = {
@@ -10,9 +29,11 @@ return {
     on_highlights = function(hl, c)
       hl.LineNr = { fg = c.comment }
       hl.CursorLineNr = { fg = c.orange, bold = true }
-      hl.Normal = { bg = c.none }
-      hl.NormalNC = { bg = c.none }
-      hl.NormalSB = { bg = c.none }
+      if require("tokyonight.config").options.transparent then
+        hl.Normal = { bg = c.none }
+        hl.NormalNC = { bg = c.none }
+        hl.NormalSB = { bg = c.none }
+      end
       hl.WinSeparator = {
         fg = c.fg_float,
         bold = true,
