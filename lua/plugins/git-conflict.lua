0530f96ac5163bbbbf11fbbe38cd3e530e674716
Yuji Kimura <kimurayuuji79@gmail.com>
2026-04-10 14:10:35 +0900

    modify

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -3,13 +3,17 @@ return {
   version = "*",
   opts = {
     default_mappings = false,
-    keymaps = {
-      ours = "<leader>Co",
-      theirs = "<leader>Ct",
-      none = "<leader>C0",
-      both = "<leader>Cb",
-      next = "<leader>Cx",
-      prev = "<leader>CX",
-    },
   },
+  config = function(_, opts)
+    require("git-conflict").setup(opts)
+    require("which-key").add({
+      { "<leader>gc", group = "Git conflict", icon = "" },
+    })
+    vim.keymap.set("n", "<leader>gco", "<cmd>GitConflictChooseOurs<CR>", { desc = "GitConflictChooseOurs" })
+    vim.keymap.set("n", "<leader>gct", "<cmd>GitConflictChooseTheirs<CR>", { desc = "GitConflictChooseTheirs" })
+    vim.keymap.set("n", "<leader>gcb", "<cmd>GitConflictChooseBoth<CR>", { desc = "GitConflictChooseBoth" })
+    vim.keymap.set("n", "<leader>gc0", "<cmd>GitConflictChooseNone<CR>", { desc = "GitConflictChooseNone" })
+    vim.keymap.set("n", "<leader>gcn", "<cmd>GitConflictNextConflict<CR>", { desc = "GitConflictNextConflict" })
+    vim.keymap.set("n", "<leader>gcp", "<cmd>GitConflictPrevConflict<CR>", { desc = "GitConflictPrevConflict" })
+  end,
 }
